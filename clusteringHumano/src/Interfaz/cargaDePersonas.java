9d971513b23d43eaef658842bce28b79b897ac61
Melanie Belén Krujoski <melaniekrujoski@gmail.com>
2020-11-09 06:49:21 -0300

    Agregado de la interfaz.

---
+++ clusteringHumano/src/Interfaz/cargaDePersonas.java
@@ -0,0 +1,224 @@
+package Interfaz;
+
+//ANOTACIONES:
+//CUANDO LOS DATOS LOS GUARDOS EN UNA TABLA, TENGO QUE IMPLEMENTAR TMB QUE ALMACENE ESO EN LA PARTE LOGICA (QUE CREE LA PERSONA)
+//CUANDO DE CLICK EN GUARDAR QUE RESETEE LOS DATOS --- ya está
+//HACER CONTADOR, CUANDO LLEGUE A LA CANTIDAD GUARDADA DESEADA, DESHABILITAR EL BOTON
+import java.awt.BorderLayout;
+
+import java.lang.Object;
+import java.awt.EventQueue;
+
+import java.nio.charset.StandardCharsets;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import java.awt.Font;
+import java.awt.List;
+import java.util.Arrays;
+
+import javax.swing.JTextField;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JDialog;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JScrollPane;
+import java.awt.Color;
+
+//@SuppressWarnings("serial")
+public class cargaDePersonas extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textFieldNombre;
+	JComboBox <String>comboBoxDeportes = new JComboBox<String>();
+	JComboBox <String>comboBoxMusica = new JComboBox<String>();
+	JComboBox <String>comboBoxEspectaculo = new JComboBox<String>();
+	JComboBox <String>comboBoxCiencia = new JComboBox<String>();
+	String [] opciones;
+	Object [] datosFilas;
+	String nombrePersona;
+	int deporte;
+	int musica;
+	int espectaculo;
+	int ciencia;
+	private JTable table;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					cargaDePersonas frame = new cargaDePersonas();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public cargaDePersonas() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(400, 250, 1050, 500); //ver despues de centrarlo bien
+		contentPane = new JPanel();
+		contentPane.setBackground(Color.WHITE);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Ingrese nombre y apellido:");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNewLabel.setBounds(38, 49, 167, 27);
+		contentPane.add(lblNewLabel);
+		
+		textFieldNombre = new JTextField();
+		textFieldNombre.setBounds(220, 51, 247, 27);
+		contentPane.add(textFieldNombre);
+		textFieldNombre.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("Seleccione un valor del 1 al 5 para las siguientes caracteristicas:");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNewLabel_1.setBounds(38, 108, 461, 40);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Interes por los deportes:");
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblNewLabel_2.setBounds(38, 168, 148, 27);
+		contentPane.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_2_1 = new JLabel("Interes por la musica:");
+		lblNewLabel_2_1.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblNewLabel_2_1.setBounds(38, 217, 148, 27);
+		contentPane.add(lblNewLabel_2_1);
+		
+		JLabel lblNewLabel_2_1_1 = new JLabel("Interes por las noticias del espectaculo:");
+		lblNewLabel_2_1_1.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblNewLabel_2_1_1.setBounds(38, 266, 220, 27);
+		contentPane.add(lblNewLabel_2_1_1);
+		
+		JLabel lblNewLabel_2_1_2 = new JLabel("Interes por la ciencia:");
+		lblNewLabel_2_1_2.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblNewLabel_2_1_2.setBounds(38, 318, 148, 27);
+		contentPane.add(lblNewLabel_2_1_2);
+		
+	
+		String [] opciones = new String[] {"1", "2", "3", "4", "5"};
+		comboBoxDeportes.setBounds(426, 171, 41, 22);
+		comboBoxDeportes.setModel(new DefaultComboBoxModel<String>(opciones));
+		contentPane.add(comboBoxDeportes);
+		
+		comboBoxMusica.setBounds(426, 217, 41, 22);
+		comboBoxMusica.setModel(new DefaultComboBoxModel<String>(opciones));
+		contentPane.add(comboBoxMusica);
+		
+		comboBoxEspectaculo.setBounds(426, 266, 41, 22);
+		comboBoxEspectaculo.setModel(new DefaultComboBoxModel<String>(opciones));
+		contentPane.add(comboBoxEspectaculo);
+		
+		//JComboBox<String> comboBoxCiencia = new JComboBox<String>();
+		comboBoxCiencia.setBounds(426, 318, 41, 22);
+		comboBoxCiencia.setModel(new DefaultComboBoxModel<String>(opciones));
+		contentPane.add(comboBoxCiencia);
+		//comboBoxCiencia.setModel(new DefaultComboBoxModel(new String[] {"1", "2", "3", "4", "5"})); 
+	
+		JButton btnNewButton_2 = new JButton("Ver grafo");
+		btnNewButton_2.setBounds(704, 381, 96, 23);
+		contentPane.add(btnNewButton_2);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(516, 49, 474, 292);
+		contentPane.add(scrollPane);
+		
+		table = new JTable(); //-------------------- TABLA
+		table.setRowSelectionAllowed(false);
+		scrollPane.setViewportView(table);
+		table.getTableHeader().setReorderingAllowed(false) ; //desabilitar mover columnas
+		
+		DefaultTableModel modelo = new DefaultTableModel();
+		
+		modelo.addColumn("Nombre");
+		modelo.addColumn("Deportes");
+		modelo.addColumn("Musica");
+		modelo.addColumn("Espectaculo");
+		modelo.addColumn("Ciencia");
+		
+		table.setModel(modelo);
+      
+
+		JButton btnNewButton = new JButton("Guardar");
+		btnNewButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if (textFieldNombre.getText().equals("")){
+					JOptionPane.showMessageDialog(null, "El campo no debe estar vacío.");				
+					}	
+				else if (esDigito(textFieldNombre.getText())){
+					JOptionPane.showMessageDialog(null, "No debe de contener números."); }	
+				else { 
+					guardarDatos();	
+					cargarTabla(table, modelo);
+					limpiarDatos();
+				}	
+			}
+		});
+		btnNewButton.setBounds(220, 381, 89, 23);
+		contentPane.add(btnNewButton);
+		}
+
+
+		private void guardarDatos() {//guarda los datos para crear el objeto Persona, porque son todos int
+			nombrePersona = textFieldNombre.getText();
+			deporte = Integer.parseInt((String)comboBoxDeportes.getSelectedItem());
+			musica = Integer.parseInt((String)comboBoxMusica.getSelectedItem());
+			espectaculo = Integer.parseInt((String)comboBoxEspectaculo.getSelectedItem());
+			ciencia = Integer.parseInt((String)comboBoxCiencia.getSelectedItem());
+			System.out.println(nombrePersona);
+			System.out.println(deporte); 	
+		}
+
+		private void cargarTabla(JTable tabla, DefaultTableModel modelo) {
+			
+			datosFilas = new Object[5]; 
+			datosFilas[0] = textFieldNombre.getText();
+			datosFilas[1] = comboBoxDeportes.getSelectedItem();
+			datosFilas[2] = comboBoxMusica.getSelectedItem();
+			datosFilas[3] = comboBoxEspectaculo.getSelectedItem();
+			datosFilas[4] = comboBoxCiencia.getSelectedItem();
+			
+			modelo.addRow(datosFilas);		
+		}
+		
+		private void limpiarDatos() { //limpia los datos por afuera cada vez que se da al boton de guardar
+			textFieldNombre.setText("");
+			comboBoxDeportes.setSelectedIndex(0);
+			comboBoxMusica.setSelectedIndex(0);
+			comboBoxEspectaculo.setSelectedIndex(0);
+			comboBoxCiencia.setSelectedIndex(0);
+		}
+		
+		private static boolean esDigito(String str) { //si da true significa que hay al menos un numero en el string
+			for (int i = 0; i < str.length(); i++) {
+				char c = str.charAt(i);
+				if (Character.isDigit(c))
+			 		return true;
+	   }
+		return false;
+	}
+		
+	
+}
---
+++ clusteringHumano/src/Interfaz/gruposFormados.java
@@ -0,0 +1,88 @@
+package Interfaz;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import java.awt.Component;
+import javax.swing.Box;
+import javax.swing.JSeparator;
+import javax.swing.JEditorPane;
+import java.awt.Color;
+import javax.swing.JTable;
+
+public class gruposFormados extends JFrame {
+
+	private JPanel contentPane;
+	private JTable tablaGrupo1;
+	private JTable tablaGrupo2;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					gruposFormados frame = new gruposFormados();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public gruposFormados() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(680, 250, 600, 500);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("GRUPO 1");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(60, 16, 159, 41);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblGrupo = new JLabel("GRUPO 2");
+		lblGrupo.setHorizontalAlignment(SwingConstants.CENTER);
+		lblGrupo.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblGrupo.setBounds(345, 16, 159, 41);
+		contentPane.add(lblGrupo);
+		
+		JSeparator separator = new JSeparator();
+		separator.setOrientation(SwingConstants.VERTICAL);
+		separator.setForeground(Color.BLACK);
+		separator.setBounds(296, 0, 2, 461);
+		contentPane.add(separator);
+		
+		JSeparator separator_1 = new JSeparator();
+		separator_1.setForeground(Color.BLACK);
+		separator_1.setBounds(0, 68, 584, 2);
+		contentPane.add(separator_1);
+		
+		tablaGrupo1 = new JTable();
+		tablaGrupo1.setBounds(10, 76, 280, 374);
+		contentPane.add(tablaGrupo1);
+		
+		DefaultTableModel modelo = new DefaultTableModel();
+		//modelo.addRow(rowData); //añadir aca de forma horizontal los nombres de los grupos (para eso tendria que tener un array de object con los nombres)
+		//tablaGrupo1.setModel(modelo);
+		
+		tablaGrupo2 = new JTable();
+		tablaGrupo2.setBounds(296, 76, 280, 374);
+		contentPane.add(tablaGrupo2);
+	}
+}
